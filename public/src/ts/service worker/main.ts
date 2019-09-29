7c6ada3fd77a0893f6ecd8ea69293a2533df6e0b
NikitaDev14 <mobidev14>
2019-09-29 22:37:21 +0300

    Intercept requests by service worker.

--- public/src/ts/service worker/main.ts
+++ public/src/ts/service worker/main.ts
@@ -1,9 +1,16 @@
-self.addEventListener('install', (event) => {
+self.addEventListener('install', (event: MessageEvent | ErrorEvent) => {
   console.log('[Service Worker] Installing Service Worker ...', event);
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', (event: ExtendableEvent) => {
   console.log('[Service Worker] Activating Service Worker ...', event);
 
   return self.Clients;
+});
+
+self.addEventListener('fetch', (event: FetchEvent) => {
+  console.log('[Service Worker] Fetching something ...', event);
+
+  //override response
+  event.respondWith(fetch(event.request));
 });

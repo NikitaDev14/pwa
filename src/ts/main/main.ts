24c634b74d9ef25f968d583c681cb2dbfe4b376b
NikitaDev14 <mobidev14>
2019-10-16 22:19:57 +0300

    GraphQL emulating and handling with 'switchMap' operator.

--- src/ts/main/main.ts
+++ src/ts/main/main.ts
@@ -1,18 +1,53 @@
+import { fromEvent } from 'rxjs';
+import { fromFetch } from 'rxjs/fetch';
+import { mergeMap, switchMap } from 'rxjs/operators';
+
 import { assignPrompt } from './feed';
 import { test } from './greet';
 
-console.log(test());
+const testBtn: HTMLButtonElement = document.querySelector('#test-btn');
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/serviceWorker.js')
-    .then(() => {
-      console.log('Service worker registered!');
-    })
-    .catch(() => {
-      console.warn('Error while service worker registering');
-    });
-}
+testBtn.addEventListener('click', test);
+
+fromEvent(testBtn, 'click')
+  .pipe(
+    switchMap(() => fromFetch(
+      'https://api.graph.cool/simple/v1/ciyz901en4j590185wkmexyex',
+      {
+        method: 'POST',
+        headers: new Headers({
+          'Content-Type': 'application/json',
+        }),
+        body: JSON.stringify({
+          query: `
+            query {
+              allUsers {
+                id
+                name
+              }
+            }
+          `,
+        }),
+      },
+    )),
+    mergeMap((rawResponse: Response) => {
+      return rawResponse.json();
+    }),
+  )
+  .subscribe((response: any) => {
+    console.log(response);
+  });
+
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker
+//     .register('/serviceWorker.js')
+//     .then(() => {
+//       console.log('Service worker registered!');
+//     })
+//     .catch(() => {
+//       console.warn('Error while service worker registering');
+//     });
+// }
 
 window.addEventListener('beforeinstallprompt', (event: any) => {
   console.log('beforeinstallprompt', event);

d37b6e6d0527574db1fbdc729d04ba6c269a249d
NikitaDev14 <mobidev14>
2019-11-03 22:20:43 +0200

    Reverted to the course project.

--- src/ts/main/main.ts
+++ src/ts/main/main.ts
@@ -1,58 +1,18 @@
-import { fromEvent } from 'rxjs';
-import { fromFetch } from 'rxjs/fetch';
-import { mergeMap, switchMap } from 'rxjs/operators';
-
 import { assignPrompt } from './feed';
 import { test } from './greet';
 
-const testBtn: HTMLButtonElement = document.querySelector('#test-btn');
+console.info(test());
 
-testBtn.addEventListener('click', test);
-
-fromEvent(testBtn, 'click')
-  .pipe(
-    switchMap(() => fromFetch(
-      'https://api.graph.cool/simple/v1/ciyz901en4j590185wkmexyex',
-      {
-        method: 'POST',
-        headers: new Headers({
-          'Content-Type': 'application/json',
-        }),
-        body: JSON.stringify({
-          query: `query userQuery($id: ID) {
-            User(id: $id) {
-              ...userFragment
-            }
-          }
-
-          fragment userFragment on User {
-            id
-            name
-          }`,
-          variables: {
-            id: 'ck1buv43a0hv201955u808iqn',
-          },
-        }),
-      },
-    )),
-    mergeMap((rawResponse: Response) => {
-      return rawResponse.json();
-    }),
-  )
-  .subscribe((response: any) => {
-    console.log(response);
-  });
-
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker
-//     .register('/serviceWorker.js')
-//     .then(() => {
-//       console.log('Service worker registered!');
-//     })
-//     .catch(() => {
-//       console.warn('Error while service worker registering');
-//     });
-// }
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/serviceWorker.js')
+    .then(() => {
+      console.log('Service worker registered!');
+    })
+    .catch(() => {
+      console.warn('Error while service worker registering');
+    });
+}
 
 window.addEventListener('beforeinstallprompt', (event: any) => {
   console.log('beforeinstallprompt', event);

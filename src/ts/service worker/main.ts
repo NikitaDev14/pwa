4af55153788ee3a7a598f8f77a9acb52bc662bf3
NikitaDev14 <mobidev14>
2019-11-17 21:21:09 +0200

    Fix refactor.

--- src/ts/service worker/main.ts
+++ src/ts/service worker/main.ts
@@ -4,83 +4,71 @@ const CACHE_DYNAMIC_NAME = 'dynamic-v1';
 self.addEventListener('install', (event: ExtendableEvent) => {
   console.log('[Service Worker] Installing Service Worker ...', event);
 
-  function saveStaticFilesToCache(cache: Cache): void {
-    console.log('[Service Worker] Precaching App Shell');
-
-    cache.addAll([
-                   '/',
-                   '/index.html',
-                   '/main.js',
-                   '/material.min.js',
-                   '/css/app.css',
-                   '/css/feed.css',
-                   '/images/main-image.jpg',
-                   'https://fonts.googleapis.com/css?family=Roboto:400,700',
-                   'https://fonts.googleapis.com/icon?family=Material+Icons',
-                   'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
-                 ]);
-  }
-
   event.waitUntil(
     caches.open(CACHE_STATIC_NAME)
-      .then(saveStaticFilesToCache),
+      .then((cache: Cache) => {
+        console.log('[Service Worker] Precaching App Shell');
+
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/main.js',
+          '/material.min.js',
+          '/css/app.css',
+          '/css/feed.css',
+          '/images/main-image.jpg',
+          'https://fonts.googleapis.com/css?family=Roboto:400,700',
+          'https://fonts.googleapis.com/icon?family=Material+Icons',
+          'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+        ]);
+      }),
   );
 });
 
 self.addEventListener('activate', (event: ExtendableEvent) => {
   console.log('[Service Worker] Activating Service Worker ...', event);
 
-  function removeAllItemsFromCache(itemKeysList: string[]): Promise<boolean[]> {
-    return Promise.all(
-      itemKeysList.map((keyItem: string) => {
-        if (keyItem !== CACHE_STATIC_NAME && keyItem !== CACHE_DYNAMIC_NAME) {
-          console.log('[Service Worker] Removing old cache.', keyItem);
-
-          return caches.delete(keyItem);
-        }
-      }),
-    );
-  }
-
   event.waitUntil(
     caches.keys()
-      .then(removeAllItemsFromCache),
+      .then((itemKeysList: string[]) => {
+        return Promise.all(
+          itemKeysList.map((keyItem: string) => {
+            if (keyItem !== CACHE_STATIC_NAME && keyItem !== CACHE_DYNAMIC_NAME) {
+              console.log('[Service Worker] Removing old cache.', keyItem);
+
+              return caches.delete(keyItem);
+            }
+          }),
+        );
+      }),
   );
 
   return self.Clients;
 });
 
 self.addEventListener('fetch', (event: FetchEvent) => {
-  function handleDynamicCache(response: Response): Response | Promise<any> {
-    if (response) {
-      return response;
-    }
-
-    const addToCache = (resp: Response, cache: Cache): Response => {
-      cache.put(event.request.url, resp.clone());
-
-      return resp;
-    };
-
-    return fetch(event.request)
-      .then((resp: Response) => {
-        return caches.open(CACHE_DYNAMIC_NAME)
-          .then(
-            addToCache.bind(null, resp),
-          );
-      })
-      .catch((error: Error) => {
-        console.warn(error);
-
-        return null;
-      });
-  }
-
   // override response
   event.respondWith(
     caches.match(event.request)
-      .then(
-        handleDynamicCache.bind(null, event),
-      ),
+      .then((response: Response): Response | Promise<any> => {
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request)
+          .then((resp: Response) => {
+            return caches.open(CACHE_DYNAMIC_NAME)
+              .then((cache: Cache): Response => {
+                cache.put(event.request.url, resp.clone());
+
+                return resp;
+              });
+          })
+          .catch((error: Error) => {
+            console.warn(error);
+
+            return null;
+          });
+        }),
   );
 });
